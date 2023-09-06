dc8420462beb6920100288d3e4eec7b8d0e169b0
Arthur Tonnoir <arthur1.tonnoir@sfr.fr>
2023-09-06 09:35:16 +0200

    feat: add Route of Adresse to the server.js :sparkles:

--- backend/server.js
+++ backend/server.js
@@ -14,16 +14,8 @@ app.use(
   })
 );
 
-const knex = require('knex')({
-    client: 'mysql2',
-    connection: {
-    host: 'localhost',
-    port: 3306,
-    user: 'webfly',
-    password: 'bLJ-9v7UFrhp(wdo',
-    database: 'webfly'
-    }
-})
+const adressRoutes = require('./routes/adresseRoutes');
+app.use("/adresses", adressRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
