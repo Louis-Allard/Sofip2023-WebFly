722c551ec7e44884f2e271ac6192a1df2363a352
Aleizy <107651178+Aleizy@users.noreply.github.com>
2023-09-07 08:43:40 +0200

    Messagerie finis

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bienvenue sur notre Messagerie !
         </a>
       </header>
     </div>
