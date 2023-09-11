035637bb2da6f203ba00ab1714389bfbdfad4dbd
Arthur Tonnoir <arthur1.tonnoir@sfr.fr>
2023-09-11 16:10:57 +0200

    fix: update userController to fix isCM of create function :bug:

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,6 +47,7 @@ exports.findById = (req, res) => {
         firstname: req.body.firstname,
         email: req.body.email,
         password: req.body.password,
+        isCM: req.body.isCM
       
     });
   
