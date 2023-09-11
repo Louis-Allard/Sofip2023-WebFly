e8fcea4230440175c08a71a2749f9b1fd4141072
Arthur Tonnoir <arthur1.tonnoir@sfr.fr>
2023-09-11 16:13:52 +0200

    fix: update CompanyRegister fix create new entreprise and adresse :bug:

--- src/pages/CompanyRegister.js
+++ src/pages/CompanyRegister.js
@@ -1,35 +1,20 @@
-
 import React, { useState } from 'react';
 import axios from 'axios';
-// import { SHA256 } from 'crypto-js';
 import '../scss/_companyreg.scss';
 
 const CompanyRegister = () => {
   const [companyName, setCompanyName] = useState('');
-  const [address, setAddress] = useState('');
-  const [street, setStreet] = useState('');
-  const [code, setCode] = useState('');
-  const [city, setCity] = useState('');
   const [siret, setSiret] = useState('');
+  const [address, setAddress] = useState({
+    id: 0,
+    numero_rue: "",
+    nom_rue: "",
+    ville: "",
+    codepostal: ""
+  });
 
   const handleChangeCompanyName = (event) => {
     setCompanyName(event.target.value);
-  };
-
-  const handleChangeAddress = (event) => {
-    setAddress(event.target.value);
-  };
-
-  const handleChangeStreet = (event) => {
-    setStreet(event.target.value);
-  };
-
-  const handleChangeCode = (event) => {
-    setCode(event.target.value);
-  };
-
-  const handleChangeCity = (event) => {
-    setCity(event.target.value);
   };
 
   const handleChangeSiret = (event) => {
@@ -38,65 +23,78 @@ const CompanyRegister = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // Envoyer une requête POST à votre endpoint d'insertion
+    postAdresse()
+  }
 
-    // Insérer la valeur dans la base de données ici
+  function postAdresse(callback){
+    axios.post('http://localhost:3001/adresses/adresse', address)
+      .then((res) => {
+        console.log('Insertion réussie');
+        let update = {id: res.data.id}
+        console.log("update :" , update)
+        setAddress(address => ({...address, ...update}))
+        console.log("adresse " , address);
+        postEntreprise()
+      })
+      .catch((error) => {
+        console.error('Erreur lors de l\'insertion', error);
+      });
+      
+  }
+
+  function postEntreprise(){
+    
     const data = {
-      v1: companyName,
-      v2: address,
-      v3: street,
-      v4: code,
-      v5: city,
-      v6: siret
+      nom: companyName,
+      siret: siret,
+      adresse: address
     };
     console.log(data);
 
-    if (data) {
       // Envoyer une requête POST à votre endpoint d'insertion
-      axios.post('http://localhost:3001/signup', data)
-        .then(response => {
-          console.log('Insertion réussie');
-        })
-        .catch(error => {
-          console.error('Erreur lors de l\'insertion', error);
-        });
-    } else {
-      console.error('Erreur : valeur non définie');
-    }
-  };
+    axios.post('http://localhost:3001/entreprises/entreprise', data)
+      .then(response => {
+        console.log('Insertion réussie');
+      })
+      .catch(error => {
+        console.error('Erreur lors de l\'insertion', error);
+      });
+  }
 
   return (
     <div className="signup">
-      <form action="http://localhost:3001/signup" method="post" onSubmit={handleSubmit} className="signup_form">
+      <form method="post" onSubmit={handleSubmit} className="signup_form">
         <h1>Saisie des informations de l'entreprise</h1>
         <div className="trivial">
           <div className="element_company">
             <label className='champForm' htmlFor="company">Nom : </label>
-            <input className='txtmailpass' type="text" name="fill_company" value={companyName} onChange={handleChangeCompanyName} required />
+            <input className='txtmailpass' type="text" name="fill_company" value={companyName} onChange={handleChangeCompanyName} />
           </div>
 
           <div className="element_address">
             <label className='champForm' htmlFor="address">N° : </label>
-            <input className='txtmailpass' type="text" name="fill_address" value={address} onChange={handleChangeAddress} required />
+            <input className='txtmailpass' type="text" name="fill_address" onChange={(e) => setAddress({ ...address, numero_rue: e.target.value })} />
           </div>
 
           <div className="element_street">
             <label className='champForm' htmlFor="street">Voie : </label>
-            <input className='txtmailpass' type="text" name="fill_street" value={street} onChange={handleChangeStreet} required />
+            <input className='txtmailpass' type="text" name="fill_street" onChange={(e) => setAddress({ ...address, nom_rue: e.target.value })} />
           </div>
 
           <div className="element_code">
             <label className='champForm' htmlFor="code">Code Postal : </label>
-            <input className='txtmailpass' type="text" name="fill_code" value={code} onChange={handleChangeCode} required />
+            <input className='txtmailpass' type="text" name="fill_code" onChange={(e) => setAddress({ ...address, codepostal: e.target.value })} />
           </div>
 
           <div className="element_city">
             <label className='champForm' htmlFor="city">Ville : </label>
-            <input className='txtmailpass' type="text" name="fill_city" value={city} onChange={handleChangeCity} required />
+            <input className='txtmailpass' type="text" name="fill_city" onChange={(e) => setAddress({ ...address, ville: e.target.value })} />
           </div>
 
           <div className="element_siret">
             <label className='champForm' htmlFor="siret">SIRET : </label>
-            <input className='txtmailpass' type="text" name="fill_siret" value={siret} onChange={handleChangeSiret} required />
+            <input className='txtmailpass' type="text" name="fill_siret" value={siret} onChange={handleChangeSiret} />
           </div>
 
           <div className='element_button'>
