35691911fe38f593bcbe320be999d884162bccc5
Arthur Tonnoir <arthur1.tonnoir@sfr.fr>
2023-09-13 08:31:47 +0200

    feat: add iduser varaible and send the variable has the component Sidebar :sparkles:

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom'
 import Sidebar from '../components/Sidebar';
 
+
 const Dashboard = () => {
+  const { iduser } = useParams()
+  
+  console.log(iduser);
     return (
-        <Sidebar />
+      <Sidebar iduser={iduser}/>
   );
 };
 
